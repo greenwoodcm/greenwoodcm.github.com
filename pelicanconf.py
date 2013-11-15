6c2dff65458be8177b9fa192775816c491cae1d6
Chris Greenwood <greenwoodcm@gmail.com>
2013-11-15 07:46:56 -0800

    added custom theme (WIP)

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,6 +5,11 @@ from __future__ import unicode_literals
 AUTHOR = u'Chris Greenwood'
 SITENAME = u'Chris Greenwood'
 SITEURL = ''
+ABOUT = 'blah blah blah'
+
+USER_IMAGE = '/images/me2.jpeg'
+
+THEME = 'custom_theme'
 
 TIMEZONE = 'Europe/Paris'
 
@@ -27,12 +32,25 @@ LINKS =  (('Pelican', 'http://getpelican.com/'),
 SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
+GITHUB_INTEGRATION_ENABLED = True
+GITHUB_USERNAME = 'greenwoodcm'
+GITHUB_URL = 'https://github.com/greenwoodcm'
+
+#TWITTER_INTEGRATION_ENABLED = True
+#TWITTER_USERNAME = 'chrismgreenwood'
+
 DEFAULT_PAGINATION = 10
 
-STATIC_PATHS = ['extra/favicon.png']
+STATIC_PATHS = [
+	'extra/favicon.png',
+	'images/me2.jpeg'
+]
 EXTRA_PATH_METADATA = {
 	'extra/favicon.png': {'path': 'favicon.ico'}
 }
+
+PLUGIN_PATH = 'pelican-plugins'
+PLUGINS = ['assets']
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
